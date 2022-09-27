880eb623faf81cd3fd0216767ca9e56d1d75b256
Justin Webb <Webb.n.justin@gmail.com>
2022-09-27 13:18:30 -0400

    Added a form to the page and used express redirect

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res, next) =>{
-    next();
+app.use('/add-product', (req, res, next) =>{
+    //form with improper html
+    res.send('<form action="/product" method="POST"> <input type="text" name="title"> <button type="submit"> Add Product </button></form>')
 });
 
-app.use('/add-product', (req, res, next) =>{
-    res.send('<h1>Add Product Page</h1>')
+app.use('/product', (req, res, next) =>{
+    //redirect with express
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) =>{
