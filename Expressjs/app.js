14ba8b381491bb8f8b7782f2b7a2612ee0bb4ca1
Justin Webb <Webb.n.justin@gmail.com>
2022-09-27 12:22:02 -0400

    learning middleware and new way to send reponses

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -1,5 +1,26 @@
 const http = require('http');
 
-const server = http.createServer();
+const express = require('express');
+
+//is an application and request handler
+const app = express();
+
+//allows you to add a new middleware function
+//accepts an array of request handlers
+//handles all incoming request
+//takes three args req, res, next
+app.use((req, res, next) =>{
+    console.log("I'm in the middleware");
+    //allow request to move on to the next middleware
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log('was i hit?');
+    //allows you attach a body of type any
+    res.send('<h1>Hello from express</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
