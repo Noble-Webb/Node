a9eb8a212d1faf4d06f2bc0f56a59a5f145f6567
Justin Webb <Webb.n.justin@gmail.com>
2022-09-27 12:31:13 -0400

    create a server using express

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 //is an application and request handler
@@ -9,18 +7,10 @@ const app = express();
 //accepts an array of request handlers
 //handles all incoming request
 //takes three args req, res, next
-app.use((req, res, next) =>{
-    console.log("I'm in the middleware");
-    //allow request to move on to the next middleware
-    next();
-});
-
 app.use((req, res, next)=>{
     console.log('was i hit?');
     //allows you attach a body of type any
     res.send('<h1>Hello from express</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
