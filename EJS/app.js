fe57c083af52bc1d59a1a9d41ea73939e55890ed
Justin Webb <Webb.n.justin@gmail.com>
2022-09-29 19:59:15 -0400

    made an ejs template version

--- EJS/app.js
+++ EJS/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -6,33 +8,13 @@ const app = express();
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-//mods imports
-const bodyParser = require('body-parser');
-const path = require('path');
-//handlebars//
-const expressHbs = require('express-handlebars');
-
-// Register the given template engine callback
-//designate layouts location
-//define default layout used for all files
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
-//handlebars//
-
-//Pug//
-//set globalconfiguration value
-//works because we installed pug and will auto register
-// app.set('view engine', 'pug');
-//set where to look for views/templates views is default
-// app.set('views', 'views');
-//Pug//
 
 
-//parser not for files
+
 app.use(bodyParser.urlencoded({extended: false}));
-//allows to statically read file/access
-//arg is the path for read access
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 //exported routes from admin
