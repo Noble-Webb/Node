0740bbce40036860bdd269e5d013ed34ac959cd5
Justin Webb <Webb.n.justin@gmail.com>
2022-10-03 18:32:40 -0400

    file storage

--- EJS/controllers/products.js
+++ EJS/controllers/products.js
@@ -14,8 +14,9 @@ exports.postAddProduct = (req, res, next) =>{
 
 exports.getProducts = (req, res, next) =>{
     //fetch products
-    const products = Product.fetchAll();
-    res.render('shop', {prods: products, docTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true
+    Product.fetchAll((products) =>{
+        res.render('shop', {prods: products, docTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true
     });
-    
-}
+});
+      
+};
--- EJS/models/product.js
+++ EJS/models/product.js
@@ -1,4 +1,6 @@
-const products = []
+const fs = require('fs');
+const path = require('path')
+const rootDir = require('../util/path');
 
 module.exports = class Product{
     constructor(title){
@@ -6,10 +8,34 @@ module.exports = class Product{
     };
 
     save(){
-        products.push(this);
+        //stores products in a file in json format
+        const p = path.join(rootDir, 'data', 'products.json');
+        //read file data
+        fs.readFile(p, (err, fileContent) =>{
+            let products = [];
+            //if no err read file
+            if(!err){
+                //read JSON
+                products =JSON.parse(fileContent);
+            };
+            products.push(this);
+            //create JSON
+            fs.writeFile(p, JSON.stringify(products), (err) =>{
+                console.log(err);
+            });
+        });
     };
-//static ensure that you can call the method on the class itself
-    static fetchAll(){
-        return products;
+    //static ensure that you can call the method on the class itself
+    static fetchAll(cb){
+        const p = path.join(rootDir, 'data', 'products.json');
+
+        fs.readFile(p, (err, fileContent) =>{
+            //if an error
+            if(err){
+                //callback to help with async
+                cb([]);
+            };
+            cb(JSON.parse(fileContent));
+        });
     };
 };
