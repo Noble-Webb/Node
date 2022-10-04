ef2ea8800f5ad692b034207440938ca75bebbff1
Justin Webb <Webb.n.justin@gmail.com>
2022-10-04 13:35:47 -0400

    added fields to the product and updated model and controller

--- EJS/controllers/admin.js
+++ EJS/controllers/admin.js
@@ -18,8 +18,13 @@ exports.getProducts = (req, res, next) =>{
 };
 
 exports.postAddProduct = (req, res, next) =>{
+    //deconstruct req body
+    const title = req.body.title
+    const imageUrl = req.body.imageUrl
+    const price = req.body.price
+    const description = req.body.description
     //creates a new product
-    const product = new Product(req.body.title);
+    const product = new Product(title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 };
